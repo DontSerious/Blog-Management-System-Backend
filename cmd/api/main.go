41b6889f088e3cb76026225ec8a921b4f2dd9026
DontSerious <1090087271@qq.com>
2023-12-31 20:26:44 +0800

    cors bug fix

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Bishe/be/cmd/api/handlers"
 	"Bishe/be/cmd/api/rpc"
+	"Bishe/be/pkg/middlewares"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,25 +16,26 @@ func Init() {
 func main() {
 	Init()
 	r := gin.Default()
+	r.Use(middlewares.Cors())
 
 	// user
-	r.POST("/login/", handlers.Login)
-	r.POST("/register/", handlers.Register)
+	r.POST("/login", handlers.Login)
+	r.POST("/register", handlers.Register)
 	r.POST("/changePWD", handlers.ChangePWD)
 	r.GET("/delUser", handlers.DelUser)
 	r.GET("/getAllUser", handlers.GetAllUser)
 
 	// userInfo
-	r.GET("/queryInfo/", handlers.Query)
-	r.POST("/updateInfo/", handlers.Update)
+	r.GET("/queryInfo", handlers.Query)
+	r.POST("/updateInfo", handlers.Update)
 
 	// edit
-	r.GET("/dirTree/", handlers.GetDirTree)
-	r.GET("/fileContent/", handlers.GetFile)
-	r.POST("/createFile/", handlers.CreateFile)
-	r.POST("/createDir/", handlers.CreateDir)
-	r.POST("/saveFile/", handlers.SaveFile)
-	r.DELETE("/delAll/", handlers.DelAll)
+	r.GET("/dirTree", handlers.GetDirTree)
+	r.GET("/fileContent", handlers.GetFile)
+	r.POST("/createFile", handlers.CreateFile)
+	r.POST("/createDir", handlers.CreateDir)
+	r.POST("/saveFile", handlers.SaveFile)
+	r.DELETE("/delAll", handlers.DelAll)
 
 	// file
 	r.POST("/uploadFile", handlers.UploadFile)
